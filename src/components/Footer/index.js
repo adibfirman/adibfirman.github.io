3e1bf712e03c83105719e7ead6c07aedc10f6043
Adib Firman <dib.firman@gmail.com>
2019-03-20 16:20:30 +0700

    refactor style footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import LogoHeart from 'react-ionicons/lib/IosHeart'
+
+import * as styles from './styles'
+
+const YEAR = new Date().getFullYear()
+const gatsbyIcon = require('../images/gatsby-icon.png')
+export function Footer() {
+  return (
+    <div css={styles.container}>
+      <span>
+        &copy; {YEAR} Made With <LogoHeart fontSize="15" /> And{' '}
+        <a
+          href="https://www.gatsbyjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={gatsbyIcon} className="gatsby-icon" alt="gatsby-icon" />
+        </a>
+      </span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,12 @@
+import { css } from '@emotion/core'
+
+export const container = css`
+  font-family: var(--font-portofolio);
+  display: flex;
+  justify-content: center;
+
+  .gatsby-icon {
+    position: relative;
+    width: 15px;
+  }
+`
