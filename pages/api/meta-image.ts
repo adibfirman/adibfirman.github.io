70021926d28b7fc96cdb446d256160ddbe6028c4
adib <dib.firman@gmail.com>
2020-10-31 11:16:22 +0700

    chore: setup meta image

--- pages/api/meta-image.ts
+++ pages/api/meta-image.ts
@@ -1,7 +1,48 @@
+/**
+ * thanks @grikomsn
+ * the origin source code from https://github.com/grikomsn/nextplate/blob/master/src/pages/api/social-image.ts
+ * with sligtly change
+ */
+import type { Browser } from "puppeteer";
 import type { NextApiHandler } from "next";
+import chrome from "chrome-aws-lambda";
+import pptr from "puppeteer";
+import qs from "querystring";
 
-const handler: NextApiHandler = (_, res) => {
-	res.end(`Status: 200`);
+const isDev = process.env.NODE_ENV === "development";
+let browser: Browser;
+
+const handler: NextApiHandler = async (req, res) => {
+	try {
+		const title = req.query.title as string;
+		const pathURL = req.query.pathURL as string;
+		const query = qs.stringify({ title, pathURL });
+		const url = `http://${req.headers.host}/meta-image?${query}`;
+
+		browser = await chrome.puppeteer.launch({
+			ignoreDefaultArgs: ["--disable-extensions"],
+			args: isDev ? [] : chrome.args,
+			defaultViewport: chrome.defaultViewport,
+			executablePath: isDev ? pptr.executablePath() : await chrome.executablePath,
+			headless: isDev ? true : chrome.headless,
+			ignoreHTTPSErrors: true
+		});
+
+		const page = await browser.newPage();
+
+		await page.setViewport({ width: 920, height: 511 });
+		await page.goto(url, { waitUntil: "load" });
+
+		const screenshot = await page.screenshot({ encoding: "binary" });
+
+		res.setHeader("content-type", "image/png");
+		res.setHeader("cache-control", "public, max-age=604800");
+		res.send(screenshot);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	} finally {
+		if (browser) await browser.close();
+	}
 };
 
 export default handler;
--- pages/meta-image.tsx
+++ pages/meta-image.tsx
@@ -1,5 +1,68 @@
 import * as React from "react";
+import Image from "next/image";
+import type { GetServerSideProps, NextPage } from "next";
+import { Heading, Flex, Text, useTheme, Grid, Box } from "@chakra-ui/core";
 
-const MetaImagePage = () => <div>sip</div>;
+type QueryStringParams = {
+	title: string;
+	pathURL: string;
+};
+
+const MetaImagePage: NextPage<QueryStringParams> = ({ title, pathURL }) => {
+	const theme = useTheme();
+
+	return (
+		<Grid
+			p="4"
+			backgroundColor="azure.50"
+			height="100vh"
+			gridTemplateRows="max-content 1fr max-content"
+		>
+			<Heading
+				textAlign="center"
+				as="h2"
+				fontFamily="grandstander"
+				fontSize="md"
+				fontWeight="normal"
+			>
+				@adibfirman
+			</Heading>
+			<Flex
+				border={`5px solid ${theme.colors.gray[500]}`}
+				borderRadius="30px"
+				overflow="hidden"
+				my="1rem"
+				position="relative"
+				p={10}
+				alignItems="center"
+			>
+				<Text color="azure.700" fontSize="6xl" maxWidth="37rem">
+					{title}
+				</Text>
+				<Box position="absolute" width="18%" right={16} top={16}>
+					<Image unsized src="/character.png" />
+				</Box>
+			</Flex>
+			<Heading
+				textAlign="center"
+				as="h2"
+				fontFamily="grandstander"
+				fontSize="md"
+				fontWeight="normal"
+			>
+				{pathURL}
+			</Heading>
+		</Grid>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps<QueryStringParams> = async ({ query }) => {
+	const title = (query.title as string) || "";
+	const pathURL = (query.pathURL as string) || "";
+
+	return {
+		props: { title, pathURL }
+	};
+};
 
 export default MetaImagePage;
