7cb1b7efcbd7bfbf64f037b162b1daf436662a24
adib <dib.firman@gmail.com>
2020-11-08 22:17:18 +0700

    separate list blogs properly

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,14 @@ import type { GetStaticProps, NextPage } from "next";
 
 import * as React from "react";
 import { Heading, Grid, useTheme } from "@chakra-ui/core";
-import { getUnixTime } from "date-fns";
-import matter from "gray-matter";
-import fs from "fs";
-import path from "path";
 
 import { Page } from "@components";
 import { NavigationCard } from "@components/UI";
-import { blogsFilePaths, BLOG_PATH } from "@utils/blogs";
+import { listBlogs } from "@utils/blogs";
 
 interface HomePageProps {
 	origin: string;
-	recentBlogs: [
-		{
-			pathname: string;
-			data: {
-				title: string;
-				spoiler: string;
-				date: string;
-			};
-		}
-	];
+	recentBlogs: typeof listBlogs;
 }
 
 const DESC_PAGE = `I'm Adib Firman, I'm software engineer from 🇮🇩 (Indonesia) day by day working and learn a fun things about web development, and occasionally write a blog too and sometimes write about what I've done learn on web things.`;
@@ -32,14 +19,7 @@ const HomePage: NextPage<HomePageProps> = ({ recentBlogs }) => {
 	const theme = useTheme();
 
 	return (
-		<Page
-			title={TITLE_PAGE + "👋"}
-			desc={DESC_PAGE}
-			SEO={{
-				title: TITLE_PAGE,
-				desc: DESC_PAGE
-			}}
-		>
+		<Page title={TITLE_PAGE + "👋"} desc={DESC_PAGE} SEO={{ title: TITLE_PAGE, desc: DESC_PAGE }}>
 			<Heading as="h2" mt={16} mb={4} fontSize="xl">
 				Recents blogs* 🇮🇩
 			</Heading>
@@ -63,29 +43,11 @@ const HomePage: NextPage<HomePageProps> = ({ recentBlogs }) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-	const listBlogs = blogsFilePaths
-		.map(blogFolder => {
-			const filePath = path.join(BLOG_PATH, blogFolder, "index.md");
-			const source = fs.readFileSync(filePath);
-			const { data, content } = matter(source);
-			const blogDate = new Date(data.date);
-
-			return {
-				content,
-				pathname: blogFolder,
-				data: {
-					title: data.title,
-					spoiler: data.spoiler,
-					date: blogDate.toDateString(),
-					timestamps: getUnixTime(blogDate)
-				}
-			};
-		})
-		.sort((a, b) => b.data.timestamps - a.data.timestamps);
+	const threeRecentBlogs = listBlogs.slice(0, 3);
 
 	return {
 		props: {
-			recentBlogs: listBlogs.slice(0, 3)
+			recentBlogs: threeRecentBlogs
 		}
 	};
 };
