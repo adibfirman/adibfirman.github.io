6c3b284a02feec9fbf43a18ecab2d3c844fc7338
adib <dib.firman@gmail.com>
2020-11-18 23:09:21 +0700

    adding google analytics and webmention

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,41 +12,59 @@ import SEO from "../next-seo.config";
 import theme from "@utils/theme";
 
 const globalStyle = css`
-	@font-face {
-		font-family: "InputMono-Medium";
-		font-style: normal;
-		font-weight: 400;
-		font-display: swap;
-		src: url("/fonts/InputMono-Medium.ttf");
-	}
+  @font-face {
+    font-family: "InputMono-Medium";
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url("/fonts/InputMono-Medium.ttf");
+  }
 `;
 
 const CustomizeApp = ({ Component, pageProps }: AppProps) => {
-	return (
-		<CacheProvider value={cache}>
-			<ThemeProvider theme={theme}>
-				<CSSReset />
-				<Global styles={globalStyle} />
-				<ColorModeProvider value="light">
-					<Head>
-						<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-						<link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
-						<link
-							href="https://fonts.googleapis.com/css2?family=Grandstander&display=swap"
-							rel="stylesheet"
-						/>
-						<link rel="preconnect" href="https://fonts.gstatic.com" />
-						<link
-							href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-							rel="stylesheet"
-						/>
-					</Head>
-					<DefaultSeo {...SEO} />
-					<Component {...pageProps} />
-				</ColorModeProvider>
-			</ThemeProvider>
-		</CacheProvider>
-	);
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Global styles={globalStyle} />
+        <ColorModeProvider value="light">
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Grandstander&display=swap"
+              rel="stylesheet"
+            />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+              rel="stylesheet"
+            />
+
+            {/* <!-- Webmention.io --> */}
+            <link rel="webmention" href="https://webmention.io/adibfirman.space/webmention" />
+            <link rel="pingback" href="https://webmention.io/adibfirman.space/xmlrpc" />
+
+            {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135917039-1" />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+
+							gtag('config', 'UA-135917039-1');
+							`
+              }}
+            />
+          </Head>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 };
 
 export default CustomizeApp;
