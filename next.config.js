f6b213963f9b5a8fdaa8eafc448d05d1aa52abbb
adib <dib.firman@gmail.com>
2020-11-18 15:01:51 +0700

    remove config `target: serverless`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  target: "serverless"
-};
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/
+});
+
+module.exports = withMDX({
+  images: {
+    deviceSizes: [420, 768, 1024, 1200],
+    iconSizes: [],
+    domains: [],
+    path: "/_next/image",
+    loader: "default"
+  }
+});
