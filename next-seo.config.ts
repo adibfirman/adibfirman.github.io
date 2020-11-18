c4639f77da8c0a46f956783a52aad10f554dc438
adib <dib.firman@gmail.com>
2020-11-18 14:44:37 +0700

    change URL production

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,21 +2,21 @@ import type { DefaultSeoProps } from "next-seo";
 
 const description = "Web Development, Software Engineering and Jr Developer For Life";
 const title = "Adib Firman";
-export const URL = "https://adibfirman-dev.dibfirman.vercel.app";
+export const URL = "https://adibfirman.space";
 
 export default {
-	title,
-	description,
-	titleTemplate: "%s · (@adibfirman)",
-	openGraph: {
-		title,
-		description,
-		url: URL,
-		type: "website"
-	},
-	twitter: {
-		cardType: "summary_large_image",
-		site: "@dibfirman",
-		handle: "@dibfirman"
-	}
+  title,
+  description,
+  titleTemplate: "%s · (@adibfirman)",
+  openGraph: {
+    title,
+    description,
+    url: URL,
+    type: "website"
+  },
+  twitter: {
+    cardType: "summary_large_image",
+    site: "@dibfirman",
+    handle: "@dibfirman"
+  }
 } as DefaultSeoProps;
