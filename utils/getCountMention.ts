7539b41e3e3c4feaff6ff8c2941b830e596d7d63
adibfirman <dib.firman@gmail.com>
2020-11-24 09:16:10 +0700

    finally fixed my webmention

--- utils/getCountMention.ts
+++ utils/getCountMention.ts
@@ -5,9 +5,8 @@ export default async function getCountMention(pathname: string) {
   const URLWeb = new URL(ownDomain);
   const target = URLWeb.origin + pathname;
   const webmentionURL = `https://webmention.io/api/mentions.jf2?per-page=1000&target=${target}`;
-  const configs = { headers: { "Content-Type": "application/json" } };
 
-  const getWebmentionCount = await fetch(webmentionURL, configs);
+  const getWebmentionCount = await fetch(webmentionURL);
   const webmention = await getWebmentionCount.json();
 
   return webmention.children.length as number;
