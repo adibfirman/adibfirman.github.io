4b830cb98751ab1b6688112a1adf8ba944bb2c5c
adibfirman <dib.firman@gmail.com>
2021-01-10 13:49:00 +0700

    remove puppeteer

--- pages/api/meta-image.ts
+++ pages/api/meta-image.ts
@@ -1,51 +1,36 @@
-/**
- * thanks @grikomsn
- * the origin source code from https://github.com/grikomsn/nextplate/blob/master/src/pages/api/social-image.ts
- * with sligtly change
- */
-import type { Browser } from "puppeteer";
 import type { NextApiHandler } from "next";
-import chrome from "chrome-aws-lambda";
-import pptr from "puppeteer";
 import qs from "querystring";
 import absoluteURL from "next-absolute-url";
 
+import { getPage } from "./_lib/chromium";
+
 const isDev = process.env.NODE_ENV === "development";
-let browser: Browser;
+let browser: any;
 
 const handler: NextApiHandler = async (req, res) => {
-	try {
-		const title = req.query.title as string;
-		const pathURL = req.query.pathURL as string;
-		const query = qs.stringify({ title, pathURL });
+  try {
+    const title = req.query.title as string;
+    const pathURL = req.query.pathURL as string;
+    const query = qs.stringify({ title, pathURL });
 
-		const { origin } = absoluteURL(req);
-		const url = `${origin}/meta-image?${query}`;
+    const { origin } = absoluteURL(req);
 
-		browser = await chrome.puppeteer.launch({
-			ignoreDefaultArgs: ["--disable-extensions"],
-			args: isDev ? [] : chrome.args,
-			defaultViewport: chrome.defaultViewport,
-			executablePath: isDev ? pptr.executablePath() : await chrome.executablePath,
-			headless: isDev ? true : chrome.headless,
-			ignoreHTTPSErrors: true
-		});
+    const page = await getPage(isDev);
+    const url = `${origin}/meta-image?${query}`;
 
-		const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 630 });
+    await page.goto(url, { waitUntil: "load" });
 
-		await page.setViewport({ width: 1200, height: 630 });
-		await page.goto(url, { waitUntil: "load" });
+    const screenshot = await page.screenshot({ encoding: "binary" });
 
-		const screenshot = await page.screenshot({ encoding: "binary" });
-
-		res.setHeader("content-type", "image/png");
-		res.setHeader("cache-control", "public, max-age=604800");
-		res.send(screenshot);
-	} catch (error) {
-		res.status(500).json({ error: error.message });
-	} finally {
-		if (browser) await browser.close();
-	}
+    res.setHeader("content-type", "image/png");
+    res.setHeader("cache-control", "public, max-age=604800");
+    res.send(screenshot);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  } finally {
+    if (browser) await browser.close();
+  }
 };
 
 export default handler;
