915423053b2c63d0004fa7e0341882e45a924d2a
adibfirman <dib.firman@gmail.com>
2021-07-10 20:06:47 +0700

    upgrade to nextjs 11

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,18 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-import type { DefaultTheme as ChakraUIDefaultTheme } from "@chakra-ui/core";
-
-type ParamsMetaImage = {
-  title: string;
-  pathURL: string;
-};
-
-type CustomizeAppProps = {
-  host: string;
-  origin: string;
-};
-
-interface CustomDefaultTheme extends ChakraUIDefaultTheme {
-  colors: ChakraUIDefaultTheme["colors"] & { dark: any };
-}
+/// <reference types="next/image-types/global" />
