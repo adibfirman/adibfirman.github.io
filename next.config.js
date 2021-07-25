764d34cdfe53e7c33e4f7495895b069a82282974
adibfirman <dib.firman@gmail.com>
2021-07-25 21:14:06 +0700

    upgrade to next 11 and using webpack 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withMDX = require("@next/mdx")({ extension: /\.mdx?$/ });
 
 module.exports = withMDX({
+  webpack5: false,
   images: {
     deviceSizes: [420, 768, 1024, 1200],
     iconSizes: [],
