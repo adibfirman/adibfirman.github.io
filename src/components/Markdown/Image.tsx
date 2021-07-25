a59737e9792852f641d67c52d21faeeb744337a3
adibfirman <dib.firman@gmail.com>
2021-07-25 22:05:03 +0700

    fix error on detail page

--- src/components/Markdown/Image.tsx
+++ src/components/Markdown/Image.tsx
@@ -1,25 +1,13 @@
-import "react-medium-image-zoom/dist/styles.css";
 import * as React from "react";
-import styled from "@emotion/styled";
 import { useRouter } from "next/router";
 import { Image as ChakraUiImage } from "@chakra-ui/react";
-import Zoom from "react-medium-image-zoom";
 import qs from "querystring";
 
-import { useDarkMode } from "@utils/useDarkMode";
-
 type Props = Partial<HTMLImageElement>;
-
-const WrapperImg = styled.div`
-  > div {
-    width: 100%;
-  }
-`;
 
 const Image = (props: Props) => {
   const [isError, setIsError] = React.useState(false);
   const router = useRouter();
-  const { bg } = useDarkMode();
 
   const fileName = props.src;
   const dirFolder = router.query.pathBlog;
@@ -28,18 +16,13 @@ const Image = (props: Props) => {
   const srcImg = !isError ? urlImage : "/not-found-img.png";
 
   return (
-    <WrapperImg>
-      <Zoom overlayBgColorStart={bg} overlayBgColorEnd={bg}>
-        <ChakraUiImage
-          onError={() => setIsError(true)}
-          cursor="pointer"
-          src={srcImg}
-          alt={typeof props.alt !== "string" ? "" : props.alt}
-          mx="auto"
-          my={4}
-        />
-      </Zoom>
-    </WrapperImg>
+    <ChakraUiImage
+      onError={() => setIsError(true)}
+      src={srcImg}
+      alt={typeof props.alt !== "string" ? "" : props.alt}
+      mx="auto"
+      my={4}
+    />
   );
 };
 
