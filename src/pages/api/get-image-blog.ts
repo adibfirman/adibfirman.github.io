f713ec20645cd8e901e0c6ef735b8dc9ade00c99
adibfirman <dib.firman@gmail.com>
2021-07-25 19:47:09 +0700

    change a way to get image blog

--- src/pages/api/get-image-blog.ts
+++ src/pages/api/get-image-blog.ts
@@ -6,10 +6,10 @@ import { BLOG_PATH } from "@utils/blogs";
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const fileName = req.query.fileName as string;
-    const dirFolder = req.query.dirFolder as string;
-    const pathImage = path.join(BLOG_PATH, dirFolder, fileName);
-    const image = fs.readFileSync(pathImage);
+    // const fileName = req.query.fileName as string;
+    // const dirFolder = req.query.dirFolder as string;
+    // const pathImage = path.join(BLOG_PATH, dirFolder, fileName);
+    // const image = fs.readFileSync(pathImage);
 
     const a = path.join(__dirname);
     console.log(a);
@@ -20,9 +20,11 @@ const handler: NextApiHandler = async (req, res) => {
       console.log(file);
     });
 
-    res.setHeader("content-type", "image/png");
-    res.setHeader("cache-control", "public, max-age=604800");
-    res.send(image);
+    res.status(200).json({ hello: "world" });
+
+    // res.setHeader("content-type", "image/png");
+    // res.setHeader("cache-control", "public, max-age=604800");
+    // res.send(image);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
