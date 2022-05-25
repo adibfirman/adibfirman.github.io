33929f7f73f99ca7fa14b74f86ac18cce318f711
adibfirman <dib.firman@gmail.com>
2022-05-25 14:54:48 +0700

    change custom domain

--- src/components/TestAd.js
+++ src/components/TestAd.js
@@ -7,7 +7,7 @@ const TestAd = () => (
     data-inventory-id="203"
     data-dimen-id="3"
     data-keywords="car charger bluetooth"
-    data-custom-domain="https://ta-staging.tokopedia.com"
+    data-custom-domain="https://1414-staging-feature.tokopedia.com"
     data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjsvuGckE1o1x3QqyL-igjSQJniKMiA5aJAhanAmGS1eX8L4tu2K2DzIXZXRE4OpUGVGmvxfGTK01XxDAqS-sq-fkMqXeJJ0-aADjWrH9U4dEHmljKGLMvyDWE3gnuVyH1T3Km3l3pXG0_AYWBpUnodudH1RP5ietsuLVlPUMLbcnaYW-fAziVw3z&amp;sig=Cg0ArKJSzN_DTPYNxbLLEAE&amp;fbs_aeid=[gw_fbsaeid]&amp;urlfix=1&amp;adurl="
     data-tracking-impression=""
   ></el>
