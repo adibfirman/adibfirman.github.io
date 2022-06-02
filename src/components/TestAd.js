64313810984f5670320d3fb8772217f44e0579e9
adibfirman <dib.firman@gmail.com>
2022-06-02 13:21:27 +0700

    remove custom tracking click

--- src/components/TestAd.js
+++ src/components/TestAd.js
@@ -8,7 +8,6 @@ const TestAd = () => (
     data-dimen-id="3"
     data-keywords="car charger bluetooth"
     data-custom-domain="https://1037-staging-feature.tokopedia.com/"
-    data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjsvuGckE1o1x3QqyL-igjSQJniKMiA5aJAhanAmGS1eX8L4tu2K2DzIXZXRE4OpUGVGmvxfGTK01XxDAqS-sq-fkMqXeJJ0-aADjWrH9U4dEHmljKGLMvyDWE3gnuVyH1T3Km3l3pXG0_AYWBpUnodudH1RP5ietsuLVlPUMLbcnaYW-fAziVw3z&amp;sig=Cg0ArKJSzN_DTPYNxbLLEAE&amp;fbs_aeid=[gw_fbsaeid]&amp;urlfix=1&amp;adurl="
     data-tracking-impression=""
   ></el>
 );
