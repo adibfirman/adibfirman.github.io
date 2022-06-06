fa1c1a22a102e97a5884d967a17bc7236c869759
adibfirman <dib.firman@gmail.com>
2022-06-06 09:57:48 +0700

    adding custom tracking

--- src/components/TestAd.js
+++ src/components/TestAd.js
@@ -8,6 +8,7 @@ const TestAd = () => (
     data-dimen-id="3"
     data-keywords="car charger bluetooth"
     data-custom-domain="https://1037-staging-feature.tokopedia.com"
+    data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjssNuGyCyLLfrl5hZ7YegE9AMgzzsHNays-J0PuhTVrkkI2-ucE8VP-s4JKK4QEkVsn9msNBHCMt8tnnMv7ZXYAaZD6-365FqrcmwekXMt96vdtmhPeIcT8UXfx_1SzQtQupct7b4piTpjOFxUF-YdbPqr0pJ4rVfJwgnlrd_1oZzvWTiW0eCkCC&sig=Cg0ArKJSzPySjmG6BYoYEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl="
     data-tracking-impression=""
   ></el>
 );
