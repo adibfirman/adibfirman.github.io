953c95f2710cbe23962714066b96b9df19e8ecbd
adibfirman <dib.firman@gmail.com>
2022-07-13 15:56:15 +0700

    test link

--- src/components/TestAd.js
+++ src/components/TestAd.js
@@ -7,7 +7,7 @@ const TestAd = () => (
     data-inventory-id="203"
     data-dimen-id="3"
     data-keywords="car charger bluetooth"
-    data-custom-domain="https://1080-staging-feature.tokopedia.com"
+    data-custom-domain="https://1205-beta-feature.tokopedia.com"
     // data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjssNuGyCyLLfrl5hZ7YegE9AMgzzsHNays-J0PuhTVrkkI2-ucE8VP-s4JKK4QEkVsn9msNBHCMt8tnnMv7ZXYAaZD6-365FqrcmwekXMt96vdtmhPeIcT8UXfx_1SzQtQupct7b4piTpjOFxUF-YdbPqr0pJ4rVfJwgnlrd_1oZzvWTiW0eCkCC&sig=Cg0ArKJSzPySjmG6BYoYEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl="
     data-tracking-impression=""
   ></el>
