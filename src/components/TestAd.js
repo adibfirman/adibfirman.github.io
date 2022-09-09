9e8cf400a0e57adca8f55e4b55c34da2e570f84e
adibfirman <dib.firman@gmail.com>
2022-09-09 14:16:58 +0700

    cookies test

--- src/components/TestAd.js
+++ src/components/TestAd.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { useCookies } from "react-cookie";
 
-const TestAd = () => (
-  <el
-    className="tdn-external-banner"
-    data-external-id="1"
-    data-inventory-id="203"
-    data-dimen-id="3"
-    // data-keywords="car charger bluetooth"
-    data-keywords="bluetooth"
-    data-custom-domain="https://78-staging-feature.tokopedia.com"
-    // data-custom-domain="https://midas-dev.tokopedia.com"
-    // data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjssNuGyCyLLfrl5hZ7YegE9AMgzzsHNays-J0PuhTVrkkI2-ucE8VP-s4JKK4QEkVsn9msNBHCMt8tnnMv7ZXYAaZD6-365FqrcmwekXMt96vdtmhPeIcT8UXfx_1SzQtQupct7b4piTpjOFxUF-YdbPqr0pJ4rVfJwgnlrd_1oZzvWTiW0eCkCC&sig=Cg0ArKJSzPySjmG6BYoYEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl="
-    // data-tracking-impression=""
-  ></el>
-);
+const TestAd = () => {
+  const [cookies] = useCookies();
+
+  return (
+    <>
+      <p>{JSON.stringify(cookies)}</p>
+      <el
+        className="tdn-external-banner"
+        data-external-id="1"
+        data-inventory-id="203"
+        data-dimen-id="3"
+        // data-keywords="car charger bluetooth"
+        data-keywords="bluetooth"
+        data-custom-domain="https://78-staging-feature.tokopedia.com"
+        // data-custom-domain="https://midas-dev.tokopedia.com"
+        // data-tracking-click="https://adclick.g.doubleclick.net/pcs/click?xai=AKAOjssNuGyCyLLfrl5hZ7YegE9AMgzzsHNays-J0PuhTVrkkI2-ucE8VP-s4JKK4QEkVsn9msNBHCMt8tnnMv7ZXYAaZD6-365FqrcmwekXMt96vdtmhPeIcT8UXfx_1SzQtQupct7b4piTpjOFxUF-YdbPqr0pJ4rVfJwgnlrd_1oZzvWTiW0eCkCC&sig=Cg0ArKJSzPySjmG6BYoYEAE&fbs_aeid=[gw_fbsaeid]&urlfix=1&adurl="
+        // data-tracking-impression=""
+      ></el>
+    </>
+  );
+};
 
 export default TestAd;
