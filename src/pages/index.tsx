3e8040535b3fd5ba87bc27ce993d1cd8b83aadee
adibfirman <dib.firman@gmail.com>
2022-12-13 06:09:47 +0700

    jixie test 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,17 +43,23 @@ const HomePage = ({ recentBlogs }: Props) => {
         </Grid>
       </Box>
       <div style={{ marginTop: "100vh" }} />
-      <div
+      {/* <div
         dangerouslySetInnerHTML={{
           __html:
-            '<script id="tkpd-ta-products-123" type="application/json">{ "trackingClick": "https://ta-staging.tokopedia.com/promo/v1/clicks/8a-xgVY2gmUEH_UpH_UdH_jpbm-xgVY789CBUsthbm-FQRo2PcB5QiUEHZFiPcBWgZUEH_yfopHdHsrN6mFiyRCsUstOHAnFbm-xgVYpyfYagZUEHpJabsK76_j7oAedHsnfosKDUSHp9fh5gaUEUMuNZM2jZJ2M33NGPMep_Mh-qMY2_1o-r7BW_sCsQABE3BPc8ujagfBvq1BRZ3BRq3JausujHsBN3jyN8Bja69Bq17jfZ32Cq1hAZSuiHsuk3Bo-ojBk__oou71p_1zzHjh9z92vzOV7_jPgHJO2H3xvzOJO_Vz0oJNEu92ouO2W_7zgPO2_Z9o-Q_BNyuPjrc-D63Wq3J-MyuPzq1Y2Z9P-q9P2ysoGrVtaQIuyH7-Nys-ZHujp1MgxuOV2_fB-P7B2PfBiH72F3s-DPuKp_MYiH7-MyuPzq1Y2Z9P-q9P2yOx3QcoXQcgjz7gXyRB-ojB2Z9o-QjNkysoGQVKaZSBiHfzE3Bo-ojBk_9xoq_e7_jz0o1NJ1_CvzJBR_M2goJB2PfBsHjNfyfO3gMHauMxsQ1N5Z325q1NJgRPHqBjO_32uHjOEz_VozcDF_uzuH7BXHA7ibm-Orfua9fBjUstiHZUDUMVDgaUEUSz5rch2rRzwyfVsqc1ibm-XP3Oig9-wQfgwy3zpUsthHmFiQSuWyMua9fVjrOYag9Ji6sJFbm-sy9zwq3zpUs2QHpgPbm-X9foxQMz2gcV7guYxgIHi6sJFHmFiy3-wPcupPmUEUjdibm-FQRo79fVDgaUEUMzBgiUDUSgBrSo2Qfdi6i-fHiUDUMoDP9o7g9-wq3zwPsUi6seOHAUObm-FrMBsguYiq3ei6s1FHAnDUMP5y3hwq3ei6soY?dv=android&r=https%3A%2F%2Fstaging.tokopedia.com%2Frainbowsstore%2Fproduk-wiena-baru-03a%3Fsrc%3Dtopads&src=recom_widget_pdp_3&page=1&management_type=1&t=android", "trackingImpression": "https://ta.tokoshorten/aFJK7" }</script>'
+            '<script id="tkpd-ta-products-123" type="application/json">{ "trackingClick": "", "trackingImpression": "https://ta.tokoshorten/aFJK7" }</script>'
         }}
-      />
-      <iframe
-        src="https://ta-staging.tokopedia.com/external/product?design=320x100&publisher_id=qqwer1234xgfd&product_id=123"
-        width="320px"
-        height="100px"
-      ></iframe>
+      /> */}
+      <a
+        href="https://ta-staging.tokopedia.com/promo/v1/clicks/8a-xgVY2gmUEH_UpH_UdH_jpbm-xgVY789CBUsthbm-FQRo2PcB5QiUEHZFiPcBWgZUEH_yfopHdHsrN6mFiyRCsUstOHAnFbm-xgVYpyfYagZUEHpJabsK76_j7oAedHsnfosKDUSHp9fh5gaUEUMuNZM2jZJ2M33NGPMep_Mh-qMY2_1o-r7BW_sCsQABE3BPc8ujagfBvq1BRZ3BRq3JausujHsBN3jyN8Bja69Bq17jfZ32Cq1hAZSuiHsuk3Bo-ojBk__oou71p_1zzHjh9z92vzOV7_jPgHJO2H3xvzOJO_Vz0oJNEu92ouO2W_7zgPO2_Z9o-Q_BNyuPjrc-D63Wq3J-MyuPzq1Y2Z9P-q9P2ysoGrVtaQIuyH7-Nys-ZHujp1MgxuOV2_fB-P7B2PfBiH72F3s-DPuKp_MYiH7-MyuPzq1Y2Z9P-q9P2yOx3QcoXQcgjz7gXyRB-ojB2Z9o-QjNkysoGQVKaZSBiHfzE3Bo-ojBk_9xoq_e7_jz0o1NJ1_CvzJBR_M2goJB2PfBsHjNfyfO3gMHauMxsQ1N5Z325q1NJgRPHqBjO_32uHjOEz_VozcDF_uzuH7BXHA7ibm-Orfua9fBjUstiHZUDUMVDgaUEUSz5rch2rRzwyfVsqc1ibm-XP3Oig9-wQfgwy3zpUsthHmFiQSuWyMua9fVjrOYag9Ji6sJFbm-sy9zwq3zpUs2QHpgPbm-X9foxQMz2gcV7guYxgIHi6sJFHmFiy3-wPcupPmUEUjdibm-FQRo79fVDgaUEUMzBgiUDUSgBrSo2Qfdi6i-fHiUDUMoDP9o7g9-wq3zwPsUi6seOHAUObm-FrMBsguYiq3ei6s1FHAnDUMP5y3hwq3ei6soY?dv=android&r=https%3A%2F%2Fstaging.tokopedia.com%2Frainbowsstore%2Fproduk-wiena-baru-03a%3Fsrc%3Dtopads&src=recom_widget_pdp_3&page=1&management_type=1&t=android"
+        target="__blank"
+        rel="noopener noreferrer"
+      >
+        <iframe
+          src="https://ta-staging.tokopedia.com/external/product?design=320x100&publisher_id=qqwer1234xgfd&product_id=123"
+          width="320px"
+          height="100px"
+        ></iframe>
+      </a>
     </Page>
   );
 };
