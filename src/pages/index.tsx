e17273ce1e36537ee96cfe882716323cd6c23fc0
adibfirman <dib.firman@gmail.com>
2022-12-21 21:20:04 +0700

    migrated on the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
+import { InferGetServerSidePropsType } from "next";
 import { Heading, Box, Grid, useTheme } from "@chakra-ui/react";
 
 import { Page } from "@components";
 import { NavigationCard } from "@components/UI";
-import { listBlogs } from "@utils/blogs";
+import { getListFromMedium } from "@utils/blogs";
 
 const DESC_PAGE = `I'm Adib Firman, I'm software engineer from 🇮🇩 (Indonesia) day-by-day working and learn a fun things about Web Ecosystem, and occasionally planting seed on my own digital garden.`;
 const TITLE_PAGE = "Hi There...!!";
 
-type Props = {
-  recentBlogs: typeof listBlogs;
+export const getServerSideProps = async () => {
+  const listBlogs = await getListFromMedium();
+  const threeRecentBlogs = listBlogs.slice(0, 3);
+
+  return {
+    props: {
+      recentBlogs: threeRecentBlogs
+    }
+  };
 };
 
-const HomePage = ({ recentBlogs }: Props) => {
+const HomePage = ({ recentBlogs }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const theme = useTheme();
 
   return (
@@ -29,30 +37,19 @@ const HomePage = ({ recentBlogs }: Props) => {
           gridTemplateColumns={[null, "repeat(3, minmax(1em, 1fr))"]}
           gap={4}
         >
-          {recentBlogs.map((blog, i) =>
-            blog ? (
-              <NavigationCard
-                key={i}
-                title={blog.data.title}
-                desc={blog.data.spoiler}
-                href={`/blog/${blog.pathname}`}
-              />
-            ) : null
-          )}
+          {recentBlogs.map(blog => (
+            <NavigationCard
+              key={blog.link}
+              title={blog.title}
+              desc={blog.content}
+              href={blog.link}
+              footerText={blog.published}
+            />
+          ))}
         </Grid>
       </Box>
     </Page>
   );
-};
-
-export const getServerSideProps = async () => {
-  const threeRecentBlogs = listBlogs.slice(0, 3);
-
-  return {
-    props: {
-      recentBlogs: threeRecentBlogs
-    }
-  };
 };
 
 export default HomePage;
