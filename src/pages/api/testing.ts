5b8a67d96a939ebdad67f8b1bfbed88c67607cb8
adibfirman <dib.firman@gmail.com>
2023-04-19 13:38:54 +0700

    init

--- src/pages/api/testing.ts
+++ src/pages/api/testing.ts
@@ -9,8 +9,8 @@ const handler: NextApiHandler = async (req, res) => {
       data: {
         adm: `
           <iframe
-            onload="window.addEventListener('message', e => {if (e.data.variant === 'tkpd-pid-${pid}-design-${design}-ready') this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-pid-${pid}-design-${design}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*'); })"
-            src="https://63-staging-feature.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
+            onload="this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-products-${pid}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*')"
+            src="https://15-beta-feature.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
             width="${width}"
             height="${height}"
           ></iframe>`
