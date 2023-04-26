f015c9a3b06734bf03b027b97e2aea3f1ba23990
adibfirman <dib.firman@gmail.com>
2023-04-26 16:56:39 +0700

    check app installed

--- src/pages/api/testing.ts
+++ src/pages/api/testing.ts
@@ -2,17 +2,14 @@ import type { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const { pid, trackingImpression, publisherID, design } = req.query;
+    const { pid, trackingImpression, publisherID, design, trackingClick } = req.query;
     const [width, height] = (design as string).split("x");
-
-    const trackingClick =
-      "https://ta-staging.tokopedia.com/promo/v1/clicks/token-token?dv=android&r=https://tokopedia.link/S0mOPhDXizb&src=recom_widget_pdp_3";
 
     res.json({
       data: {
         adm: `
           <iframe
-            onload="window.addEventListener('message', e => {if (e.data.variant === 'tkpd-pid-${pid}-design-${design}-ready') this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-pid-${pid}-design-${design}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*'); })"
+            onload="window.addEventListener('message', e => {if (e.data.variant === 'tkpd-pid-${pid}-design-${design}-ready') this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-pid-${pid}-design-${design}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*'); if(e.data.type === 'app-check-result') alert(JSON.stringify(e.data)) })"
             src="https://63-staging-feature.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
             width="${width}"
             height="${height}"
