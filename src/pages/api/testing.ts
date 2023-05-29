cffdcea7801b7003884d319d8f807d9f54c1a25f
adibfirman <dib.firman@gmail.com>
2023-05-29 19:41:28 +0700

    prep

--- src/pages/api/testing.ts
+++ src/pages/api/testing.ts
@@ -2,19 +2,36 @@ import type { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const { pid, trackingClick, trackingImpression, publisherID, design } = req.query;
+    const {
+      pid,
+      trackingClick,
+      trackingImpression,
+      trackingNonViewport,
+      publisherID,
+      design
+    } = req.query;
     const [width, height] = (design as string).split("x");
 
     res.json({
       data: {
         adm: `
-          <iframe
-            onload="this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-products-${pid}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*')"
-            src="https://15-beta-feature.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
-            width="${width}"
-            height="${height}"
-          ></iframe>`
+        <iframe
+          onload="window.addEventListener('message', e => {if (e.data.variant === 'tkpd-pid-${pid}-design-${design}-ready') this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-prd-${pid}-design-${design}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}', trackingNonViewport: '${trackingNonViewport}' } }), '*'); })"
+          src="https://midas-dev.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
+          width="${width}"
+          height="${height}"
+        ></iframe>
+        `
       }
+      // data: {
+      //   adm: `
+      //     <iframe
+      //       onload="this.contentWindow.postMessage(JSON.stringify({ variant: 'tkpd-products-${pid}', data: { trackingClick: '${trackingClick}', trackingImpression: '${trackingImpression}' } }), '*')"
+      //       src="https://15-beta-feature.tokopedia.com/external/product?design=${design}&publisher_id=${publisherID}&product_id=${pid}"
+      //       width="${width}"
+      //       height="${height}"
+      //     ></iframe>`
+      // }
     });
   } catch (err) {
     // res.status(500).json({ error: err.message });
