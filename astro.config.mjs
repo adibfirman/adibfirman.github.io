6a0995e4279c727b92a8e3c6b74ead761f84bf75
adibfirman <dib.firman@gmail.com>
2025-05-13 16:12:22 +0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
---
+++ src/env.d.ts
@@ -0,0 +1,5 @@
+// Custom types declarations
+declare var myString: string;
+
+// Astro types, not necessary if you already have a `tsconfig.json`
+/// <reference path="../.astro/types.d.ts" />
