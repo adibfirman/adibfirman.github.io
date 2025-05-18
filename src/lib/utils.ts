f0bf59ff7d929b7f11bccfeeeab527c0f14460aa
adibfirman <dib.firman@gmail.com>
2025-05-18 14:49:44 +0700

    init blog list

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { readdir, readFile } from "fs/promises";
+import matter from "gray-matter";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,4 +14,40 @@ export function formatDate(dateString: string): string {
     day: "numeric",
   };
   return new Date(dateString).toLocaleDateString("en-US", options);
+}
+
+export async function getListBlog() {
+  try {
+    const entries = await readdir("./public/blog", { withFileTypes: true });
+    const dirs = entries
+      .filter((entry) => entry.isDirectory())
+      .map((entry) => entry.name);
+
+    const fileContents = await Promise.all(
+      dirs.map((dir) =>
+        readFile("./public/blog/" + dir + "/index.mdx", "utf8"),
+      ),
+    );
+
+    const posts = dirs.map((slug, i) => {
+      const fileContent = fileContents[i];
+      const { data } = matter(fileContent);
+      const { date, draft, title, excerpt, tags } = data;
+
+      return {
+        slug,
+        date: String(date),
+        draft: Boolean(draft),
+        title: String(title),
+        excerpt: String(excerpt),
+        tags: tags || [],
+      };
+    });
+
+    posts.sort((a, b) => (Date.parse(a.date) < Date.parse(b.date) ? 1 : -1));
+
+    return posts.filter((post) => !post.draft);
+  } catch (err) {
+    return [];
+  }
 }
