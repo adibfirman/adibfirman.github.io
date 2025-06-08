d17f8e21a0c039126be687b0a58ba30df874df52
adibfirman <dib.firman@gmail.com>
2025-06-08 21:54:01 +0700

    remove contact path

--- src/lib/menus.ts
+++ src/lib/menus.ts
@@ -2,5 +2,4 @@ export const MENUS = [
   { path: "/", label: "About" },
   { path: "/blog", label: "Blog" },
   { path: "/til", label: "TIL" },
-  { path: "/contact", label: "Contact" },
 ] as const;
