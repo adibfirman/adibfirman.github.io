336bee9cef919cd9bbe0e3044289285c06f5afb0
adibfirman <dib.firman@gmail.com>
2025-08-27 11:33:22 +0700

    init tailwind custom var css

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -21,14 +21,12 @@ export function Welcome() {
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
+            <p className="leading-6 text-center">What&apos;s next?</p>
             <ul>
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
                   <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                    className="group flex items-center gap-3 self-stretch p-3 leading-normal hover:underline"
                     href={href}
                     target="_blank"
                     rel="noreferrer"
